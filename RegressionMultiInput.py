1f21362b8724b1fa8f723bd8a46ef8867d5808f3
gvanhemert <91330305+gvanhemert@users.noreply.github.com>
2021-10-07 10:31:16 +0200

    Update RegressionMultiInput.py

--- RegressionMultiInput.py
+++ RegressionMultiInput.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Conv2DTranspose, Reshape
 from tensorflow.keras.layers import Activation, Dropout, Dense, Flatten, Input, UpSampling2D, Concatenate
 from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
@@ -11,6 +12,7 @@ import tensorflow as tf
 import pickle5 as pickle
 from tensorflow.keras import backend as K
 import locale
+import math
 
 def load_dataset(path="/content/drive/MyDrive/DeepLearning/DataEmoda.npy"):
     with open(path, "rb") as fh:
@@ -30,17 +32,21 @@ def create_input(df, input_labels):
 
 def create_mlp(dim):
     model = Sequential()
+    #model.add(Dense(16, input_dim=dim, activation="relu"))
     model.add(Dense(64, input_dim=dim, activation='relu'))
+    #model.add(Dense(256, activation='relu'))
     model.add(Dense(1024, activation='relu'))
+    #model.add(Dense(4096, activation='relu'))
+    #model.add(Dense(16384, activation='relu'))
     model.add(Dense(256*256, activation='relu'))
     model.build((None, 256*256))
 
     return model
 
-def create_cnn(width, height, depth):
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
     inputShape = (height, width, depth)
+    chanDim = -1
     
-    #Main Branch
     inputs = Input(shape=inputShape)
     x = Conv2D(64, (3,3), padding="same")(inputs)
     x = Activation("relu")(x)
@@ -94,7 +100,7 @@ def create_cnn(width, height, depth):
     
     model = Model(inputs, x)
     
-    return model
+    return model 
     
 def full_model(cnn_model, mlp_model):
     
@@ -127,6 +133,10 @@ def full_model(cnn_model, mlp_model):
     x = Activation("relu")(x)
     x = BatchNormalization()(x)
     
+    x = Conv2D(16, (3,3), padding="same")(x)
+    x = Activation("relu")(x)
+    x = BatchNormalization()(x)
+
     x = UpSampling2D(size=(2,2))(x)
     x = Conv2DTranspose(1, (3,3), padding="same", activation="linear")(x)
     
@@ -137,14 +147,23 @@ def full_model(cnn_model, mlp_model):
 
 df = load_dataset()
 (inputImages,outputImages) = create_input_output(df, 'bathy', 'hs')
-(inputImages, outputImages) = ((inputImages - np.nanmean(inputImages))/np.nanstd(inputImages), (outputImages - np.nanmean(outputImages)/np.nanstd(outputImages)))
+inStd = np.nanstd(inputImages)
+inMean = np.nanmean(inputImages)
+inputImages = (inputImages - np.nanmean(inputImages))/np.nanstd(inputImages)
+#(inputImages, outputImages) = ((inputImages - np.nanmean(inputImages))/np.nanstd(inputImages), (outputImages - np.nanmean(outputImages)/np.nanstd(outputImages)))
 
 inputAttr = create_input(df, ['$\eta$', '$\zeta$','$\theta_{wave}$'])
 inputAttr[:,0] = (inputAttr[:,0] - np.mean(inputAttr[:,0])) / np.std(inputAttr[:,0])
 inputAttr[:,1] = (inputAttr[:,1] - np.mean(inputAttr[:,1])) / np.std(inputAttr[:,1])
+#inputAttr[:,0] = inputAttr[:,0] / 50.
+#inputAttr[:,1] = inputAttr[:,1] / 20.
 inputAttr[:,2] = inputAttr[:,2] / (2*np.pi)
 
-(inputImages, outputImages) = (np.nan_to_num(inputImages,nan=-10.), np.nan_to_num(outputImages, nan=-10.))
+
+#inputImages = inputImages / 1950. #min bathy
+#outputImages = outputImages / 20. #max hs
+
+(inputImages, outputImages) = (np.nan_to_num(inputImages,nan=-1.), np.nan_to_num(outputImages, nan=-1.))
 
 split = train_test_split(inputImages, inputAttr, outputImages, test_size=0.25, random_state=42)
 (trainImgX, testImgX, trainAttrX, testAttrX, trainY, testY) = split
@@ -156,10 +175,17 @@ model = full_model(cnn_model, mlp_model)
 opt = Adam(learning_rate=1e-4, decay=1e-4/200)
 model.compile(loss="mean_squared_error", optimizer=opt)
 
+initial_learning_rate = 0.001
+def lr_step_decay(epoch, lr):
+    drop_rate = 0.5
+    epochs_drop = 20.0
+    return initial_learning_rate * math.pow(drop_rate, math.floor(epoch/epochs_drop))
+
 print("[INFO] training model...")
 model.fit(x=[trainImgX, trainAttrX], y=trainY,
           validation_data=([testImgX, testAttrX], testY),
           epochs=200, batch_size=8)
+          #callbacks=[LearningRateScheduler(lr_step_decay, verbose=1)])
 
 model.save('/content/drive/MyDrive/DeepLearning/ModelV6')
 
